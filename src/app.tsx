1d397b7e26d50818ab8c10d7890d9d5cf7e70766
So魚丸丷 <dygood@outlook.com>
2020-05-07 13:36:45 +0800

    修复一个错误.

--- src/app.tsx
+++ src/app.tsx
@@ -89,7 +89,7 @@ class App extends React.Component<AppProps, AppState> {
             this.ibeacons[exist].count += 1;
           }
         }
-        this.setGlobal({ allowUpdate: true });
+        this.setGlobal({ allowUpdate: true, hadFail: false });
       }
     });
   };
