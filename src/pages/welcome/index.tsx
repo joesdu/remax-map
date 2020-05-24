af4dfe86dd14a2f7e79fd6f6dccc380477c42717
杜宇 <dygood@outlook.com>
2020-05-24 22:13:55 +0800

    提交一下

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -91,7 +91,7 @@ class Welcome extends React.Component<WelcomeProps, WelcomeState> {
       <View className={styles.app}>
         {this.renderIntoButton()}
         <View className={styles.viewFooter}>
-          <View className={styles.footerLink}>Insider Preview 20200522.1100</View>
+          <View className={styles.footerLink}>Insider Preview 20200524.2200</View>
           <View className={styles.txtVersion}>Copyright © 2020 WinSide. All Rights Reserved.</View>
         </View>
       </View>
